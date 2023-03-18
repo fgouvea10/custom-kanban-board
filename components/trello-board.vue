fe28bf018c5984316720e921b895147d868f5ef8
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-03-18 12:03:32 -0300

    feat: create trello board component

---
+++ components/trello-board.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { nanoid } from 'nanoid'
+import type { Column } from '~~/types';
+
+const columns = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: 'Test',
+    tasks: [
+      { id: nanoid(), title: 'Create something', createdAt: new Date()}
+    ]
+  }
+])
+</script>
+
+<template>
+  <div>
+    <div v-for="column in columns" :key="column.id">
+      <header>
+        {{ column.title }}
+      </header>
+      <p v-for="task in column.tasks" :key="task.id">
+        {{ task.title }}
+      </p>
+    </div>
+  </div>
+</template>
